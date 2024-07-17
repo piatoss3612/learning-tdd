d3d5e083840804016300ae6f4c6f991455bae630
piatoss <crewe1746@naver.com>
2024-07-17 10:39:18 +0900

    refactor: moved Money and Portfolio to stocks package

--- go/money_test.go
+++ go/money_test.go
@@ -1,91 +1,41 @@
 package main
 
-import "testing"
-
-type Money struct {
-	amount   float64
-	currency string
-}
-
-func (m Money) times(multiplier float64) Money {
-	return Money{
-		amount:   m.amount * multiplier,
-		currency: m.currency,
-	}
-}
-
-func (m Money) Divide(divisor float64) Money {
-	return Money{
-		amount:   m.amount / divisor,
-		currency: m.currency,
-	}
-}
+import (
+	s "tdd/stocks"
+	"testing"
+)
 
 func TestMultiplication(t *testing.T) {
-	fiver := Money{
-		amount:   5,
-		currency: "USD",
-	}
-	tenner := fiver.times(2)
-	expectedTenner := Money{
-		amount:   10,
-		currency: "USD",
-	}
+	fiver := s.NewMoney(5, "USD")
+	tenner := fiver.Times(2)
+	expectedTenner := s.NewMoney(10, "USD")
 	assertEqual(t, expectedTenner, tenner)
 }
 
-func TestMultiplicationInEuros(t *testing.T) {
-	tenEuros := Money{
-		amount:   10,
-		currency: "EUR",
-	}
-	twentyEuros := tenEuros.times(2)
-	expectedTwentyEuros := Money{
-		amount:   20,
-		currency: "EUR",
-	}
-	assertEqual(t, expectedTwentyEuros, twentyEuros)
-}
-
 func TestDivision(t *testing.T) {
-	originalMoney := Money{amount: 4002, currency: "KRW"}
+	originalMoney := s.NewMoney(4002, "KRW")
 	actualMoneyAfterDivision := originalMoney.Divide(4)
-	expectedMoneyAfterDivision := Money{amount: 1000.5, currency: "KRW"}
+	expectedMoneyAfterDivision := s.NewMoney(1000.5, "KRW")
 	assertEqual(t, expectedMoneyAfterDivision, actualMoneyAfterDivision)
 }
 
-func assertEqual(t *testing.T, expected, actual Money) {
-	if actual != expected {
-		t.Errorf("Expected %v, got %v", expected, actual)
-	}
-}
-
-type Portfolio []Money
-
-func (p Portfolio) Add(money Money) Portfolio {
-	return append(p, money)
-}
-
-func (p Portfolio) Evaluate(currency string) Money {
-	total := 0.0
-	for _, m := range p {
-		total += m.amount
-	}
-
-	return Money{amount: total, currency: currency}
-}
-
 func TestAddition(t *testing.T) {
-	var portfolio Portfolio
-	var portfolioInDollars Money
+	var portfolio s.Portfolio
+	var portfolioInDollars s.Money
 
-	fiveDollars := Money{amount: 5, currency: "USD"}
-	tenDollars := Money{amount: 10, currency: "USD"}
-	fifteenDollars := Money{amount: 15, currency: "USD"}
+	fiveDollars := s.NewMoney(5, "USD")
+	tenDollars := s.NewMoney(10, "USD")
+	fifteenDollars := s.NewMoney(15, "USD")
 
 	portfolio = portfolio.Add(fiveDollars)
 	portfolio = portfolio.Add(tenDollars)
 	portfolioInDollars = portfolio.Evaluate("USD")
 
 	assertEqual(t, fifteenDollars, portfolioInDollars)
+}
+
+func assertEqual(t *testing.T, expected, actual s.Money) {
+	if actual != expected {
+		t.Errorf("Expected %v, got %v", expected, actual)
+	}
 }
