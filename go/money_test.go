9f6455e66eda2e08942da01545259fcf9943a42f
piatoss <crewe1746@naver.com>
2024-07-18 13:28:22 +0900

    feat: conversion of Money from EUR to USD

--- go/money_test.go
+++ go/money_test.go
@@ -34,6 +34,20 @@ func TestAddition(t *testing.T) {
 	assertEqual(t, fifteenDollars, portfolioInDollars)
 }
 
+func TestAdditionDollarsAndEuros(t *testing.T) {
+	var portfolio s.Portfolio
+
+	fiveDollars := s.NewMoney(5, "USD")
+	tenEuros := s.NewMoney(10, "EUR")
+
+	portfolio = portfolio.Add(fiveDollars)
+	portfolio = portfolio.Add(tenEuros)
+
+	expectedValue := s.NewMoney(17, "USD")
+	actualValue := portfolio.Evaluate("USD")
+	assertEqual(t, expectedValue, actualValue)
+}
+
 func assertEqual(t *testing.T, expected, actual s.Money) {
 	if actual != expected {
 		t.Errorf("Expected %v, got %v", expected, actual)
--- go/stocks/portfolio.go
+++ go/stocks/portfolio.go
@@ -9,8 +9,18 @@ func (p Portfolio) Add(money Money) Portfolio {
 func (p Portfolio) Evaluate(currency string) Money {
 	total := 0.0
 	for _, m := range p {
-		total += m.amount
+		total += convert(m, currency)
 	}
 
 	return Money{amount: total, currency: currency}
+}
+
+func convert(m Money, currency string) float64 {
+	eurToUsd := 1.2
+
+	if m.currency == currency {
+		return m.amount
+	}
+
+	return m.amount * eurToUsd
 }
